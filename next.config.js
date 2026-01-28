ea28e3a7c9f91f237bd99266337ba888171185da
Kohei Kameyama <kohei.kameyama@avatouch.net>
2026-01-28 19:46:17 +0900

    Fix: Update next.config.js to use serverExternalPackages instead of deprecated experimental option

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
-  },
+  serverExternalPackages: ['@prisma/client', 'prisma'],
 }
 
 module.exports = nextConfig
