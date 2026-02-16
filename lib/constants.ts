fc1a42181f461380dbd73b785014c90e9ab5e70b
Kohei Kameyama <kohei.kameyama@avatouch.net>
2026-02-16 13:33:05 +0900

    chore: ポートフォリオ・ウォッチリストの5銘柄制限を一時的に解除

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,9 +2,9 @@
  * アプリケーション全体で使用する定数
  */
 
-// 銘柄管理の制限
-export const MAX_PORTFOLIO_STOCKS = 5
-export const MAX_WATCHLIST_STOCKS = 5
+// 銘柄管理の制限（一時的に解除中）
+export const MAX_PORTFOLIO_STOCKS = 100
+export const MAX_WATCHLIST_STOCKS = 100
 
 // データ取得の制限
 export const MAX_PASSED_STOCKS_RETRIEVE = 20
