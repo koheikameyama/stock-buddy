995420a1bfe59614e10947f85b187f7e84340782
Kohei Kameyama <kohei.kameyama@avatouch.net>
2026-02-22 12:26:15 +0900

    feat: create translation files for i18n

---
+++ locales/ja/index.ts
@@ -0,0 +1,19 @@
+import common from './common.json';
+import portfolio from './portfolio.json';
+import stocks from './stocks.json';
+import analysis from './analysis.json';
+import notifications from './notifications.json';
+import settings from './settings.json';
+import auth from './auth.json';
+import news from './news.json';
+
+export default {
+  common,
+  portfolio,
+  stocks,
+  analysis,
+  notifications,
+  settings,
+  auth,
+  news,
+};
