27617c68538b5785aac9cf74d83d38c328a3ae6d
Kohei Kameyama <kohei.kameyama@avatouch.net>
2026-02-22 12:23:19 +0900

    feat: install and configure next-intl

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const withNextIntl = require('next-intl/plugin')('./lib/i18n.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   serverExternalPackages: ['@prisma/client', 'prisma'],
 }
 
-module.exports = nextConfig
+module.exports = withNextIntl(nextConfig);
